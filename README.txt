377507e076163fa343a5862579ed38f6c17b269f
Sylvain Viollon <thefunny@gmail.com>
2008-05-20 09:17:01 +0000

    - Add support for captcha.

--- README.txt
+++ README.txt
@@ -4,6 +4,6 @@ silva.pas.openid
 
 Simple PAS extension for integration of OpenID in Silva.
 
-Module based on plone.openid.
+This module is based on plone.openid.
 
 
--- silva/pas/openid/browser/views.py
+++ silva/pas/openid/browser/views.py
@@ -2,24 +2,41 @@
 # See also LICENSE.txt
 # $Id$
 
+from zope.component import getMultiAdapter
+
 from silva.pas.openid.interfaces import IOpenIDAskUserInformation
 
 from Products.Five import BrowserView
 
 from zExceptions import Redirect
+from urllib import quote
 
 class StartOpenIDRegistration(BrowserView):
+
+    def _redirect(self, msg):
+        root = self.context.get_root()
+        args = (root.absolute_url(), quote(msg))
+        next_url = '%s/silva_openid_register.html?openid_status=%s' % args
+        raise Redirect, next_url
 
     def __call__(self):
         """Register a new user to use OpenID.
         """
 
         root = self.context.get_root()
-            
+
         id = self.request.form.get('__ac_identity_register', '').strip()
         if not id:
-            next_url = root.absolute_url() + '/silva_openid_register.html'
-            raise Redirect, next_url
+            self._redirect("You need to provide an identity.")
+            
+        captcha = self.request.form.get('__ac_identity_register_captcha', None)
+        if not captcha:
+            self._redirect("You need to fill the captcha.")
+
+        utility = getMultiAdapter((self.context, self.request), name='captcha')
+        if not utility.verify(captcha):
+            self._redirect("Error while validating the captcha.")
+
 
         self.request.form['__ac_identity_url'] = id # Re-inject the id
 
